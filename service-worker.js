47db815323889ccec94007a9b070d58fa80192f7
HMtrading <mehedihassan.mh10@gmail.com>
2025-03-22 22:12:20 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,17 @@
-self.addEventListener('install', function(event) {
-    console.log('Service Worker installing.');
+self.addEventListener("install", (event) => {
+    console.log("Service Worker Installed");
+    event.waitUntil(
+        caches.open("static").then((cache) => {
+            return cache.addAll(["/index.html", "/manifest.json"]);
+        })
+    );
 });
 
-self.addEventListener('activate', function(event) {
-    console.log('Service Worker activating.');
+self.addEventListener("fetch", (event) => {
+    console.log("Fetching:", event.request.url);
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
